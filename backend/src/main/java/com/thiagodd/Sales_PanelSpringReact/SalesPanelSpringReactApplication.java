e4dbce5478665b30de55ee5b0eb2c5ddecc66697
= <=>
2022-11-24 07:36:49 -0300

    Project created

---
+++ backend/src/main/java/com/thiagodd/Sales_PanelSpringReact/SalesPanelSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodd.Sales_PanelSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPanelSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesPanelSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thiagodd/Sales_PanelSpringReact/SalesPanelSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodd.Sales_PanelSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPanelSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
