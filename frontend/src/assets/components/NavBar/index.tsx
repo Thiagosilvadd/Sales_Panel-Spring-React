7712b68f2815dbba340b9447c06991746b68db6a
Thiagodd <silvaa.thiagod2@gmail.com>
2022-11-30 08:52:55 -0300

    Home, navigation

--- frontend/src/assets/components/NavBar/index.tsx
+++ frontend/src/assets/components/NavBar/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ImgThiagoDD from 'assets/img/ThiagoDD.svg'
+import {Link} from "react-router-dom";
 
 
 const NavBar = () => {
     return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+        <div
+            className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgThiagoDD} alt="DevSuperior" width="200" />
+                    <Link to="/">
+                        <img src={ImgThiagoDD} alt="DevSuperior" width="200"/>
+                    </Link>
                 </nav>
             </div>
         </div>
